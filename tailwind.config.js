841720521fec690d6cf0c014239d86f76255620f
Zahra Ebrahimi <zahra.ea90@gmail.com>
2023-09-23 00:10:12 +0330

    lessons5-8

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
